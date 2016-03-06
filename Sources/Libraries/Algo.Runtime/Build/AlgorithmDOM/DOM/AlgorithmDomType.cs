75a9597d9dd635f7588eb976e62624da74b87eec
Etienne BAUDOUX <ebaudoux@hotmail.fr>
2016-03-06 16:42:05 +0100

    Bug fixed + big performance improvement with BinaryOperator + general performance improvement.

---
+++ Sources/Libraries/Algo.Runtime/Build/AlgorithmDOM/DOM/AlgorithmDomType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Runtime.Build.AlgorithmDOM.DOM
+{
+    internal enum AlgorithmDomType
+    {
+        ExpressionStatement,
+        AssignStatement,
+        BinaryOperatorExpression,
+        ClassConstructorDeclaration,
+        ClassDeclaration,
+        ClassMethodDeclaration,
+        ClassPropertyDeclaration,
+        ClassReferenceExpression,
+        ConditionStatement,
+        EntryPointMethod,
+        Identifier,
+        InstanciateExpression,
+        InvokeCoreMethodExpression,
+        InvokeMethodExpression,
+        IterationStatement,
+        ParameterDeclaration,
+        PrimitiveExpression,
+        Program,
+        PropertyReferenceExpression,
+        ReturnStatement,
+        SnippetExpression,
+        SnippetStatement,
+        ThisReferenceExpression,
+        VariableDeclaration,
+        VariableReferenceExpression
+    }
+}
---
+++ Sources/Libraries/Algo.Runtime/ComponentModel/OperatorHelper/OperatorHelperCache.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Algo.Runtime.Build.AlgorithmDOM.DOM;
+
+namespace Algo.Runtime.ComponentModel.OperatorHelper
+{
+    internal static class OperatorHelperCache
+    {
+        #region Structures
+
+        internal class Method
+        {
+            internal MethodInfo MethodInfo { get; set; }
+            internal Type[] ParametersType { get; set; }
+        }
+
+        #endregion
+
+        #region Fields
+
+        private static List<Method> _methods;
+
+        #endregion
+
+        #region Methods
+
+        internal static void Initialize()
+        {
+            _methods = new List<Method>();
+            foreach (var methodInfo in typeof(OperatorHelper).GetRuntimeMethods().ToList())
+            {
+                var parameters = methodInfo.GetParameters();
+                if (parameters.Length != 2)
+                {
+                    continue;
+                }
+
+                var method = new Method
+                {
+                    MethodInfo = methodInfo,
+                    ParametersType = new[] {parameters[0].ParameterType, parameters[1].ParameterType}
+                };
+
+                _methods.Add(method);
+            }
+        }
+
+        internal static MethodInfo GetOperator(AlgorithmBinaryOperatorType operatorType, Type leftType, Type rightType)
+        {
+            var operatorTypeString = operatorType.ToString();
+            var method = _methods.FirstOrDefault(m => m.MethodInfo.Name == operatorTypeString && m.ParametersType[0] == leftType && m.ParametersType[1] == rightType);
+            if (method == null)
+            {
+                return null;
+            }
+            return method.MethodInfo;
+        }
+
+        #endregion
+    }
+
+}
